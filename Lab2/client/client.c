eaf47c161f48ea9dc0e66abbdcc104887b5a8a2b
Joshua Eckels <eckelsjd@rose-hulman.edu>
2020-04-26 15:36:47 -0400

    can send and receive large files now. Working on non-text files now...

--- Lab2/client/client.c
+++ Lab2/client/client.c
@@ -350,27 +350,55 @@ int main(int argc, char** argv) {
                     exit(1);
                 }
 
-                // receive file from server
-                // assume file size is below 1024 for now
-                memset(buf,0,MAXDATASIZE+2);
-                bytes_recv = recv(sockfd, buf, MAXDATASIZE+1,0);
-                if (bytes_recv == -1) {
-                    perror("client: recv");
-                    exit(1);
-                }
-                buf[bytes_recv] = '\0';
-                printf("Received file from server.\n");
-
                 // save file to disk
                 printf("Writing to disk...\n");
-                FILE *fd = fopen(fullpath, "w");
+                FILE *fd = fopen(fullpath, "a");
                 if (fd == NULL) {
                     printf("Failed to open file\n");
                     exit(1);
                 }
-                if (fputs(buf, fd) < 0) {
-                    perror("Failed during file write");
-                    exit(1);
+
+                // block for 5 sec until data is visible on server socket
+                fd_set rfds;
+                struct timeval tv;
+                int retval;
+
+                FD_ZERO(&rfds);
+                FD_SET(sockfd,&rfds);
+                tv.tv_sec = 5;
+                tv.tv_usec = 0;
+
+                retval = select(sockfd+1,&rfds,NULL,NULL,&tv);
+
+                if (retval == -1) {
+                    perror("select()");
+                } else if (retval) {
+                    printf("Data is available now\n");
+                } else {
+                    printf("No data within five seconds.\n");
+                    break;
+                }
+ 
+                // receive arbitrarily large file from server
+                // server must send full file at once (one send() call)
+                while (1) {
+                    memset(buf,0,MAXDATASIZE+2);
+                    bytes_recv = recv(sockfd, buf, MAXDATASIZE+1,MSG_DONTWAIT);
+                    if (bytes_recv == -1) {
+                        // catch if recv would have blocked
+                        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                            printf("No more bytes to read. Exiting loop...\n");
+                            break;
+                        }
+                        perror("client: recv");
+                        exit(1);
+                    }
+                    buf[bytes_recv] = '\0';
+
+                   if (fputs(buf, fd) < 0) {
+                        perror("Failed during file write");
+                        exit(1);
+                    }
                 }
                 fclose(fd);
                 free(fullpath);
